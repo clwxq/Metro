fdc6ccb159b68d4279e0319ebacdd41ad5b437b4
Patrik Čermák <patrik.cermak@educhem.cz>
2023-06-12 10:39:00 +0200

    Přidejte soubory projektu.

---
+++ Metro/LinkaMetra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro
+{
+    internal class LinkaMetra
+    {
+        public string name { get; set; }
+        bool active { get; set; }
+        string[] stops { get; set; }
+    }
+}
---
+++ Metro/Program.cs
@@ -0,0 +1,17 @@
+﻿#region pole
+string[] A = { "Depo Hostivař", "Skalka", "Strašnická", "Želivského", "Flora", "Jiřího z Poděbrad", 
+    "Náměstí Míru", "Muzeum", "Můstek", "Staroměstská", "Malostranská", "Hradčanská", "Dejvická", 
+    "Bořislavka", "Nádraží Veleslavín", "Petřiny", "Nemocnice Motol" };
+
+string[] B = { "Zličín", "Stodůlky", "Luka", "Lužiny", "Hůrka", "Nové Butovice", "Jinonice", 
+    "Radlická", "Smíchovské nádraží", "Anděl", "Karlovo náměstí", "Národní třída", "Můstek", 
+    "Náměstí republiky", "Florenc", "Křižíkova", "Invalidovna", "Palmovka", "Českomoravská", 
+    "Vysočanská", "Kolbenova", "Hloubětín", "Rajská zahrada", "Černý most" };
+
+string[] C = { "Letňany", "Prosek", "Střížkov", "Ládví", "Kobylisy", "Nádraží Holešovice", 
+    "Vltavská", "Florenc", "Hlavní nádraží", "Muzeum","I. P. Pavlova", "Vyšehrad", "Pražského povstání",
+    "Pankrác", "Budějovická", "Kačerov", "Roztyly", "Chodov", "Opatov", "Háje" };
+
+string[] D = { "Depo Písnice", "Písnice", "Libuš", "Nové Dvory", "Nemocnice Krč", "Nádraží Krč",
+        "Olbrachtova", "Pankrác", "Náměstí Bratří Synků", "Náměstí Míru" };
+#endregion
